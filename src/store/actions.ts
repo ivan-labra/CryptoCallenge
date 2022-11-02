7915e5a092b3bff366af5e1c3c6b3c6d71373c80
vova45 <94209953+vova45@users.noreply.github.com>
2022-11-02 12:03:58 -0300

    dispatchs

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,15 +1,13 @@
 import { API_URL } from 'react-native-dotenv';
-import { Crypto } from '../interface/index';
+
 export const ADD_CRYPTOS = 'ADD_CRYPTOS';
 
 export const addCryptos: Function = () => {
-  return async (
-    dispacht: (arg0: { type: string; payload: Crypto[] }) => void
-  ) => {
+  return async dispatch => {
     try {
       const res = await fetch(API_URL);
       const resJson = await res.json();
-      dispacht({ type: ADD_CRYPTOS, payload: resJson.data });
+      dispatch({ type: ADD_CRYPTOS, payload: resJson.data });
     } catch (error) {
       console.log(error);
     }
